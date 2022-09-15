f2c1cf7c003b58e0986810cfb10aecd4cfefa28d
amirullah <amir.pnl08@gmail.com>
2022-09-15 10:48:24 +0700

    tes

---
+++ coba/index.php
@@ -0,0 +1,20 @@
+<?php 
+echo "Ini adalah script PHP pertama <br>";
+echo "tes1";
+$baru1 = "<br>Ini adalah variable baru";
+$baru2 = 123456+1000;
+echo $baru1."<b> dan </b>".$baru2;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3><?php echo $baru2; ?></h3>
+</body>
+</html>
